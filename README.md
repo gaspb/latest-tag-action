3f0f3aa80eee6c9851cc62d38c7689c7432ad255
gasp <gaspard.boursin@gmail.com>
2022-10-27 13:45:09 +0200

    add version

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Based on https://github.com/actions-ecosystem/action-get-latest-tag 
 
 Adapted to extract a version from a tag with a prefix
+
+outputs : 
+- tag : the full tag, including prefix
+- version : tag without the prefix 
--- entrypoint.sh
+++ entrypoint.sh
@@ -16,17 +16,21 @@ else
 fi
 
 latest_tag=''
+latest_version=''
 
 for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
     tag="${ref#refs/tags/}"
     if echo "${tag}" | grep -Eq "${to_grep}"; then
-      latest_tag="${tag#*${INPUT_PREFIX}}"
+      latest_tag="${tag}"
+      latest_version="${tag#*${INPUT_PREFIX}}"
       break
     fi
 done
 
 if [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; then
   latest_tag="${INPUT_INITIAL_VERSION}"
+  latest_version="${INPUT_INITIAL_VERSION}"
 fi
 
 echo "tag=${latest_tag}" >> "$GITHUB_OUTPUT"
+echo "version=${latest_version}" >> "$GITHUB_OUTPUT"
